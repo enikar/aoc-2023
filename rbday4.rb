c051643cafd1eba9333ad69bc3922a39665d5e59
enikar <enikar@gresille.org>
2023-12-13 23:09:16 +0100

    Symplify the class Card

--- rbday4.rb
+++ rbday4.rb
@@ -7,28 +7,16 @@ require 'strscan'
 # a Card is represent as the card number and the score of the card.
 class Card
   def initialize(str)
-    @card, wins, nums = parse_card(str)
-    @score = wins.reduce(0) do |acc, n|
-      if nums.include?(n)
-        (acc + 1)
-      else
-        acc
-      end
-    end
-  end
-  attr_reader :card, :score
-
-  def parse_card(str)
     s = StringScanner.new(str)
     s.skip(/^Card +/)
-    card = s.scan(/\d+/).to_i
+    @card = s.scan(/\d+/).to_i
     s.skip(/: +/)
     wins = s.scan(/(\d+ +)+/).split(/ +/).collect(&:to_i)
     s.skip(/\| +/)
     nums = s.scan(/(\d+ *)+$/).split(/ +/).collect(&:to_i)
-    [card, wins, nums]
+    @score = wins.intersection(nums).size
   end
-  private :parse_card
+  attr_reader :card, :score
 end
 
 def part1(cards)
